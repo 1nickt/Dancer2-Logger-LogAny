3801de22f665b2ab0a03fb86ebd92e02a8632432
Nick Tonkin <nick@nicktonkin.net>
2017-09-18 09:31:09 -0400

    Version upgrade

--- lib/Dancer2/Logger/LogAny.pm
+++ lib/Dancer2/Logger/LogAny.pm
@@ -1,6 +1,6 @@
 package Dancer2::Logger::LogAny;
 # ABSTRACT: Use Log::Any to control logging from your Dancer2 app
-our $VERSION = '0.9901';
+our $VERSION = '0.9902';
 
 use strict; use warnings;
 use Dancer2 qw/ :syntax !log !debug !info !notice !warning !error /;
